e4237b4a50770c2bb7542cc4229fc0a62736e77b
Callum Quigley <super-callum@hotmail.com>
2018-03-29 12:10:01 +0100

    started dev

---
+++ BookingManager/BookingManager/BookingManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BookingManager
+{
+    class BookingManager
+    {
+        static void Main(string[] args)
+        {
+            //started dev @ 12:10
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
