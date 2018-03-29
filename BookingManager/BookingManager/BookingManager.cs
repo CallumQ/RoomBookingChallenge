81e6196f25de5cd0c2266a6fbf27e05cb56c9fb8
Callum Quigley <super-callum@hotmail.com>
2018-03-29 12:12:22 +0100

    created booking class

--- BookingManager/BookingManager/BookingManager.cs
+++ BookingManager/BookingManager/BookingManager.cs
@@ -1,13 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BookingManager
 {
     class BookingManager
     {
+
+        private List<Booking> bookingList = new List<Booking>();
         static void Main(string[] args)
         {
             //started dev @ 12:10
-            Console.WriteLine("Hello World!");
+            
+
+
+
+        }
+    }
+
+
+    class Booking {
+
+        private DateTime startTime;
+        private DateTime endTime;
+
+        public Booking(DateTime startTime, DateTime endTime)
+        {
+            if (startTime < endTime)
+            {
+
+                this.startTime = startTime;
+                this.endTime = endTime;
+            }
+
+            else {
+                throw new Exception("Start time must be before end time");
+
+            }
         }
     }
 }
